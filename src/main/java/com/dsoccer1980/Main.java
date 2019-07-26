4465d018ed95e9fe3fceac6e7b8faa1bb6d46115
dsoccer1980 <dsoccer1980@gmail.com>
2019-07-26 20:50:43 +0300

    Remove unnecessary code from Main

--- src/main/java/com/dsoccer1980/Main.java
+++ src/main/java/com/dsoccer1980/Main.java
@@ -2,34 +2,11 @@ package com.dsoccer1980;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
-
-
-        ConfigurableApplicationContext context = SpringApplication.run(Main.class);
-//        UserRepository userRepository = context.getBean(UserRepository.class);
-//        Optional<User> teremok = userRepository.findByName("Teremok");
-//        teremok.ifPresent(System.out::println);
-//        userRepository.save(new User("Denis"));
-//        User user = userRepository.findById(1).get();
-//
-//        StackImpl stack = context.getBean(StackImpl.class);
-//
-//        stack.reset(user);
-//        stack.push(user, 7);
-//        stack.push(user, 1);
-//        stack.push(user, 3);
-//        System.out.println(stack.view(user));
-//
-//        System.out.println(stack.pop(user));
-//        System.out.println(stack.pop(user));
-//
-//        System.out.println(stack.view(user));
-
-
+        SpringApplication.run(Main.class);
     }
 }
